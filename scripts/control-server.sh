07e6c81202f24037ac0ef1c7c273b562d9a260cc
Erik J. Olson <hello@erikjolson.com>
2023-05-02 16:29:28 -0500

    Move command processing into function

--- scripts/control-server.sh
+++ scripts/control-server.sh
@@ -1,30 +1,28 @@
-#!/bin/bash
-
-## echo "on" | nc cube-3186 4444
-
-PORT=4444
-CUBE_URL="http://cube#touch"
+function runCommand {
+    if [ "$1" == 'reboot' ]; then
+        sudo reboot
+    elif [ "$1" == "on" ]; then
+        DISPLAY=:0 xdotool mousemove 10 10 && DISPLAY=:0 xdotool mousemove 0 0 && DISPLAY=:0 xdotool click 1
+    elif [ "$1" == "off" ]; then
+        xset -display :0 dpms force off
+    elif [ "$1" == "start" ]; then
+        export DISPLAY=:0.0 && firefox --kiosk "$CUBE_URL" &
+        sleep 10
+        runCommand on
+    elif [ "$1" == "stop" ]; then
+        killall chrome
+    elif [ "$1" == "restart" ]; then
+        killall chrome
+        runCommand start
+    elif [ "$1" == "maintenance" ]; then
+        killall chrome
+        export DISPLAY=:0.0 && firefox -url "$CUBE_URL" &
+    fi
+}
 
 echo "Cube listening for commands on port $PORT..."
 
 while read line; do
     echo "Received '$line' command..."
-
-    if [ "$line" == 'reboot' ]; then
-        sudo reboot
-    elif [ "$line" == "on" ]; then
-        DISPLAY=:0 xdotool mousemove 10 10 && DISPLAY=:0 xdotool mousemove 0 0 && DISPLAY=:0 xdotool click 1
-    elif [ "$line" == "off" ]; then
-        xset -display :0 dpms force off
-    elif [ "$line" == "start" ]; then
-        export DISPLAY=:0.0 && firefox --kiosk "$CUBE_URL" &
-    elif [ "$line" == "stop" ]; then
-        killall firefox
-    elif [ "$line" == "restart" ]; then
-        killall firefox
-        export DISPLAY=:0.0 && firefox --kiosk "$CUBE_URL" &
-    elif [ "$line" == "maintenance" ]; then
-        killall firefox
-        export DISPLAY=:0.0 && firefox -url "$CUBE_URL" &
-    fi
+    runCommand "$line"
 done < <(nc -k -l $PORT)
